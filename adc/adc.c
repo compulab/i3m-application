c60291ed4ba9803c4f7f1c127707c03ad0073e07
Arkadi Gurkov <arkadi.gurkov@compulab.co.il>
2016-02-11 10:59:57 +0200

    ADC: adc update fix
    adc makes average of POWER_COUNT times adc samples.
    The problem was that adc didn't wait to get the new sample before taking the value.
    
    Signed-off-by: Arkadi Gurkov <arkadi.gurkov@compulab.co.il>

--- adc/adc.c
+++ adc/adc.c
@@ -12,10 +12,15 @@
 #define MY_ADC    ADCA
 #define MY_ADC_CH ADC_CH0
 
+#define POWER_COUNT		1000
+
+uint16_t power_log[POWER_COUNT];
 long current_power;
 char current_power_str[7];
 float gain = 0;
 float vcc = 3.3;
+
+
 
 void adc_init()
 {
@@ -41,23 +46,40 @@ long adc_avg()
 	uint32_t i;
 	uint16_t adca1_result = 0;
 	uint32_t power_sum = 0;
-	for (i=0; i < 1000 ;i++) {
+	for (i=0; i < POWER_COUNT ;i++) {
 		adc_start_conversion(&MY_ADC, MY_ADC_CH);  //one conversion begins
+		adc_wait_for_interrupt_flag(&MY_ADC, MY_ADC_CH);
 		adca1_result = adc_get_result(&MY_ADC, MY_ADC_CH) & 0x3FFF;
+		power_log[i] = adca1_result;
 		power_sum = power_sum + adca1_result;
 	}
 
 	return power_sum / (i);
 }
 
+void print_adc_log(long adc_avg)
+{
+	uart_send_string_working("\n\r======== ADC log =====\n\r");
+	uart_send_num_working(POWER_COUNT, 10);
+	uart_send_string_working(" samples:\n\r");
+	for (int i = 0; i < POWER_COUNT; i++) {
+		uart_send_num_working(power_log[i], 10);
+		uart_send_string_working(", ");
+	}
+	uart_send_string_working("\n\r adc avg: ");
+	uart_send_num_working(adc_avg, 10);
+	uart_send_string_working(" adc result: ");
+	uart_send_num_working(current_power, 10);
+}
+
 void set_power_data(char *str)
 {
-	if (current_power_state != POWER_ON) {
-		sprintf(str, "0 W");
-	} else {
-		current_power = adc_avg() * 0.06378 * 2; //0.06378 /// (gain * gain);// * 158.34);//* 0.177; //*0.07731r P =79.17 * V_adc , P/GAIN = 158.34 //
+	long avg;
+
+		avg = adc_avg();
+		current_power = avg * 0.06378 * 2; //0.06378 /// (gain * gain);// * 158.34);//* 0.177; //*0.07731r P =79.17 * V_adc , P/GAIN = 158.34 //
 		current_power = current_power * 0.778 + 15.8;
-		sprintf(str, "%ld W  ", current_power);
-	}
+		print_adc_log(avg);
+		sprintf(str, "%ld W", current_power);
 	strcpy(current_power_str, str);
 }
