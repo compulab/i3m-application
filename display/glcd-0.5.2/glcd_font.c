dc2dbd2604f66c6a02b85fa12926b330699d9054
Arkadi Gurkov <arkadi.gurkov@compulab.co.il>
2016-02-17 10:24:20 +0200

    GLCD: default fonts fix
    Fixed number and types of default fonts
    
    Signed-off-by: Arkadi Gurkov <arkadi.gurkov@compulab.co.il>

--- display/glcd-0.5.2/glcd_font.c
+++ display/glcd-0.5.2/glcd_font.c
@@ -20,83 +20,19 @@ static  struct glcd_FontConfig_t arial_black_11x11_font = {
 		.table_type = MIKRO,
 };
 
-static  struct glcd_FontConfig_t bebas_neue_18x36_num_font = {
-		.font_table = Bebas_Neue18x36_Numbers,
-		.width = 18,
-		.height = 36,
+static  struct glcd_FontConfig_t courier_new_14x18_num_font = {
+		.font_table = Courier_New14x18_NUM,
+		.width = 14,
+		.height = 18,
 		.start_char = 48,
 		.end_char = 57,
 		.table_type = MIKRO,
 };
 
-static  struct glcd_FontConfig_t bebas_neue_20x36_bold_num_font = {
-		.font_table = Bebas_Neue20x36_Bold_Numbers,
-		.width = 20,
-		.height = 36,
-		.start_char = 48,
-		.end_char = 57,
-		.table_type = MIKRO,
-};
-
-static  struct glcd_FontConfig_t earthbound_12x19_num_font = {
-		.font_table = Earthbound_12x19_48to57,
-		.width = 12,
-		.height = 19,
-		.start_char = 48,
-		.end_char = 57,
-		.table_type = MIKRO,
-};
-
-static  struct glcd_FontConfig_t liberation_sans_11x14_num_font = {
-		.font_table = Liberation_Sans11x14_Numbers,
-		.width = 11,
-		.height = 14,
-		.start_char = 48,
-		.end_char = 57,
-		.table_type = MIKRO,
-};
-
-static  struct glcd_FontConfig_t liberation_sans_15x21_num_font = {
-		.font_table = Liberation_Sans15x21_Numbers,
-		.width = 15,
-		.height = 21,
-		.start_char = 48,
-		.end_char = 57,
-		.table_type = MIKRO,
-};
-
-static  struct glcd_FontConfig_t liberation_sans_17x17_alpha_font = {
-		.font_table = Liberation_Sans17x17_Alpha,
-		.width = 17,
-		.height = 17,
-		.start_char = 65,
-		.end_char = 90,
-		.table_type = MIKRO,
-};
-
-static  struct glcd_FontConfig_t liberation_sans_20x28_num_font = {
-		.font_table = Liberation_Sans20x28_Numbers,
-		.width = 20,
-		.height = 28,
-		.start_char = 48,
-		.end_char = 57,
-		.table_type = MIKRO,
-};
-
-static  struct glcd_FontConfig_t liberation_sans_27x36_num_font = {
-		.font_table = Liberation_Sans27x36_Numbers,
-		.width = 27,
-		.height = 36,
-		.start_char = 48,
-		.end_char = 57,
-		.table_type = MIKRO,
-};
-
-
-static  struct glcd_FontConfig_t times_new_roman_11x12_font = {
-		.font_table = Times_New_Roman11x12,
-		.width = 11,
-		.height = 12,
+static  struct glcd_FontConfig_t courier_new_8x13_bold_font = {
+		.font_table = Courier_New8x13,
+		.width = 8,
+		.height = 13,
 		.start_char = 32,
 		.end_char = 127,
 		.table_type = MIKRO,
@@ -111,50 +47,33 @@ static  struct glcd_FontConfig_t sysfont_5x7_font = {
 		.table_type = MIKRO,
 };
 
-struct glcd_FontConfig_t *get_font_by_type(enum glcd_font_type type)
+struct glcd_FontConfig_t *get_font_by_type(uint8_t font_id)
 {
-	struct glcd_FontConfig_t *font;
-	switch (type) {
-	case GLCD_FONT_ARIAL_11X11_BOLD:
-		font = &arial_black_11x11_font;
-		break;
-	case GLCD_FONT_BEBAS_NEUE_18X36_NUM:
-		font = &bebas_neue_18x36_num_font;
-		break;
-	case GLCD_FONT_BEBAS_NEUE_20X36_BOLD_NUM:
-		font = &bebas_neue_20x36_bold_num_font;
-		break;
-	case GLCD_FONT_EARTHBOUND_12X19_NUM:
-		font = &earthbound_12x19_num_font;
-		break;
-	case GLCD_FONT_LIBERATION_SANS_11X14_NUM:
-		font = &liberation_sans_11x14_num_font;
-		break;
-	case GLCD_FONT_LIBERATION_SANS_15X21_NUM:
-		font = &liberation_sans_15x21_num_font;
-		break;
-	case GLCD_FONT_LIBERATION_SANS_17X17_ALPHA:
-		font = &liberation_sans_17x17_alpha_font;
-		break;
-	case GLCD_FONT_LIBERATION_SANS_20X28_NUM:
-		font = &liberation_sans_20x28_num_font;
-		break;
-	case GLCD_FONT_LIBERATION_SANS_27X36_NUM:
-		font = &liberation_sans_27x36_num_font;
-		break;
-	case GLCD_FONT_TIMES_NEW_ROMAN_11X12:
-		font = &times_new_roman_11x12_font;
-		break;
-	case GLCD_FONT_SYSFONT_5X7:
-		font = &sysfont_5x7_font;
-		break;
-	default:
-		font = &sysfont_5x7_font;
-		break;
+	if (font_id > font_size)
+		return NULL;
+	else if (font_id > NUM_DEFAULT_FONTS)
+		return fonts[font_id];
+	else {
+		struct glcd_FontConfig_t *font;
+		switch (font_id) {
+		case GLCD_FONT_ARIAL_11X11_BOLD:
+			font = &arial_black_11x11_font;
+			break;
+		case GLCD_FONT_COURIER_NEW_8X13_BOLD:
+			font = &courier_new_8x13_bold_font;
+			break;
+		case GLCD_FONT_COURIER_NEW_14X18_NUM:
+			font = &courier_new_14x18_num_font;
+			break;
+		case GLCD_FONT_SYSFONT_5X7:
+			font = &sysfont_5x7_font;
+			break;
+		default:
+			font = &sysfont_5x7_font;
+			break;
+		}
+		return font;
 	}
-//	if (font != NULL)
-//		glcd_set_font_from_font(font);
-	return font;
 }
 
 void glcd_add_font(struct glcd_FontConfig_t *new_font, uint16_t font_id)
--- display/glcd-0.5.2/glcd_font.h
+++ display/glcd-0.5.2/glcd_font.h
@@ -12,35 +12,21 @@
 
 //#include "glcd.h"
 #include "glcd_text.h"
-#include "fonts/Bebas_Neue18x36_Numbers.h"
-#include "fonts/Bebas_Neue20x36_Bold_Numbers.h"
-#include "fonts/Earthbound_12x19_48to57.h"
-#include "fonts/Liberation_Sans11x14_Numbers.h"
-#include "fonts/Liberation_Sans15x21_Numbers.h"
-#include "fonts/Liberation_Sans17x17_Alpha.h"
-#include "fonts/Liberation_Sans20x28_Numbers.h"
-#include "fonts/Liberation_Sans27x36_Numbers.h"
+#include "fonts/Courier_New_32_to_127_8X13.h"
+#include "fonts/Courier_New_48_to_57_14X18_Num.h"
 #include "fonts/arial_black_32_to_127_11X11.h"
 #include "fonts/font5x7.h"
-#include "fonts/times_new_roman_32_to_127_11X12.h"
 
-#define NUM_DEFAULT_FONTS		11
+#define NUM_DEFAULT_FONTS		4
 
 enum glcd_font_type {
 	GLCD_FONT_SYSFONT_5X7,
+	GLCD_FONT_COURIER_NEW_8X13_BOLD,
+	GLCD_FONT_COURIER_NEW_14X18_NUM,
 	GLCD_FONT_ARIAL_11X11_BOLD,
-	GLCD_FONT_TIMES_NEW_ROMAN_11X12,
-	GLCD_FONT_BEBAS_NEUE_18X36_NUM,
-	GLCD_FONT_BEBAS_NEUE_20X36_BOLD_NUM,
-	GLCD_FONT_EARTHBOUND_12X19_NUM,
-	GLCD_FONT_LIBERATION_SANS_11X14_NUM,
-	GLCD_FONT_LIBERATION_SANS_15X21_NUM,
-	GLCD_FONT_LIBERATION_SANS_17X17_ALPHA,
-	GLCD_FONT_LIBERATION_SANS_20X28_NUM,
-	GLCD_FONT_LIBERATION_SANS_27X36_NUM,
 };
 
-struct glcd_FontConfig_t *get_font_by_type(enum glcd_font_type type);
+struct glcd_FontConfig_t *get_font_by_type(uint8_t font_id);
 
 void glcd_add_font(struct glcd_FontConfig_t *new_font, uint16_t font_id);
 
