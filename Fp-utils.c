1c89ff4848f4ae88086f27ef0211bff741d4831d
Nikita Kiryanov <nikita@compulab.co.il>
2017-02-11 21:47:47 +0200

    timer: move screen_task to Fp-utils
    Fp-utils isn't really the proper place for screen_task. Ideally we would
    have a subsystem that handles logical display stuff like this, but until
    we have that, we shall move the task code to the same place where the
    function it calls is defined, and refactor it later.
    
    Signed-off-by: Nikita Kiryanov <nikita@compulab.co.il>

--- Fp-utils.c
+++ Fp-utils.c
@@ -1193,6 +1193,19 @@ void update_info()
 	}
 }
 
+static struct work update_screen_work = { .do_work = update_info };
+
+#define UPDATE_SCREEN_TIME		1
+static double screen_get_recur_period(void)
+{
+    return UPDATE_SCREEN_TIME;
+}
+
+struct scheduler_task screen_sec_task = {
+    .work = &update_screen_work,
+    .get_recur_period = screen_get_recur_period,
+};
+
 #define UPDATE_REQ_SEC	1
 
 static struct work requests_work = { .do_work = update_requests };
--- timer/subdir.mk
+++ timer/subdir.mk
@@ -4,24 +4,21 @@ C_SRCS += \
 ./timer/tc_scheduler.c \
 ./timer/rtc_scheduler.c \
 ./timer/ambient_task.c \
-./timer/screen_saver_task.c \
-./timer/screen_task.c
+./timer/screen_saver_task.c
 
 OBJS += \
 ./timer/tc.o \
 ./timer/tc_scheduler.o \
 ./timer/rtc_scheduler.o \
 ./timer/ambient_task.o \
-./timer/screen_saver_task.o \
-./timer/screen_task.o
+./timer/screen_saver_task.o
 
 C_DEPS += \
 ./timer/tc.d \
 ./timer/tc_scheduler.d \
 ./timer/rtc_scheduler.d \
 ./timer/ambient_task.d \
-./timer/screen_saver_task.d \
-./timer/screen_task.d
+./timer/screen_saver_task.d
 
 # Each subdirectory must supply rules for building sources it contributes
 timer/%.o: timer/%.c
