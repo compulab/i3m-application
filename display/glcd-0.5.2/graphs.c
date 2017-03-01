b3a82a0b03603a20df79abf3d0184ba2c210b6ac
Nikita Kiryanov <nikita@compulab.co.il>
2017-03-01 21:30:14 +0200

    display: graphs: fix whitespace
    Get rid of trailing whitespace.

--- display/glcd-0.5.2/graphs.c
+++ display/glcd-0.5.2/graphs.c
@@ -2,7 +2,7 @@
    \file graphs.c
    \brief Functions relating to graphs. e.g bar graphs etc.
    \author Andy Gock
- */ 
+ */
 
 /*
 	Copyright (c) 2012, Andy Gock
@@ -49,7 +49,7 @@ void glcd_bar_graph_horizontal_no_border(uint8_t x, uint8_t y, uint8_t width, ui
 {
 	if (height < 3) {
 		return;
-	}	
+	}
 	glcd_fill_rect(x, y, glcd_map(0,width,val), height , BLACK);
 }
 
@@ -68,10 +68,10 @@ void glcd_scrolling_bar_graph(uint8_t x, uint8_t y, uint8_t width, uint8_t heigh
 {
 	uint8_t nx, ny;
 	uint8_t color;
-	
+
 	/* Draw border of graph */
 	glcd_draw_rect(x,y,width,height,BLACK);
-	
+
 	/* Scroll inner contents left by one pixel width */
 	for (ny = 1; ny <= (height-2); ny++) {
 		/* Redraw each horizontal line */
@@ -80,18 +80,18 @@ void glcd_scrolling_bar_graph(uint8_t x, uint8_t y, uint8_t width, uint8_t heigh
 			glcd_set_pixel(x+nx,y+ny,color);
 		}
 	}
-	
+
 	val = val * (height-3) / 255;
-	
+
 	/* Make sure we're not exceeding the size of box interior */
 	if (val > (height-3)) {
 		val = height - 3;
 	}
-	
+
 	/* Draw new bar - both black and white portions*/
 	glcd_draw_line(x+width-2,y+height-2,x+width-2,y+height-2-val,BLACK);
 	glcd_draw_line(x+width-2,y+height-3-val,x+width-2,y+1,WHITE);
-	
+
 	/* Write to display */
 	glcd_write();
 }
@@ -102,13 +102,13 @@ static void glcd_scrolling_bar_graph_timing(uint8_t x, uint8_t y, uint8_t width,
 	if (line_width == 0) {
 		line_width = 1;
 	}
-	
+
 	/* Adjust graph line's width by just running glcd_scrolling_bar_graph() x number of times */
 	/* \todo This should be done differently! */
 	for (n=0; n<line_width; n++) {
 		glcd_scrolling_bar_graph(x,y,width,height,val);
 	}
-	
+
 	if (delay) {
 		delay_ms(delay);
 	}
@@ -116,5 +116,5 @@ static void glcd_scrolling_bar_graph_timing(uint8_t x, uint8_t y, uint8_t width,
 
 static uint8_t glcd_map(uint8_t x1, uint8_t x2, uint8_t x)
 {
-	return x1+(x2-x1)*x/255;	
+	return x1+(x2-x1)*x/255;
 }
