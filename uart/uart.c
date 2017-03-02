d6536d68870eed0d99d01d5147dafa883f78ebce
Nikita Kiryanov <nikita@compulab.co.il>
2017-03-02 16:22:46 +0200

    uart: cleanup code
    Fix whitespace and coding style transgressions and hide internal defines
    from the "API" in .h.
    
    No functional changes.

--- uart/uart.c
+++ uart/uart.c
@@ -9,6 +9,8 @@
 
 #ifdef UART_DEBUG
 
+#define BSEL 	16
+#define UART_BASE	USARTD0
 
 void uart_init(void)
 {
@@ -18,7 +20,7 @@ void uart_init(void)
 	PORTD.DIRSET = PIN3_bm;
 
 	UART_BASE.BAUDCTRLA = BSEL;
-	UART_BASE.BAUDCTRLB = (0 << USART_BSCALE0_bp)|(BSEL >> 8);
+	UART_BASE.BAUDCTRLB = (0 << USART_BSCALE0_bp) | (BSEL >> 8);
 
 	UART_BASE.CTRLC = USART_CHSIZE_8BIT_gc | USART_PMODE_DISABLED_gc;
 	UART_BASE.CTRLB = USART_TXEN_bm | USART_RXEN_bm ;
@@ -29,19 +31,16 @@ char num_str[10];
 void uart_send_char(char c)
 {
 	if (c == '\0')
-		return ;
-    while( !(UART_BASE.STATUS & USART_DREIF_bm) ); //Wait until DATA buffer is empty
+		return;
 
-    UART_BASE.DATA = c;
-
+	while( !(UART_BASE.STATUS & USART_DREIF_bm) ); //Wait until DATA buffer is empty
+	UART_BASE.DATA = c;
 }
 
 void uart_send_string(char *text)
 {
     while(*text)
-    {
     	uart_send_char(*text++);
-    }
 }
 
 void uart_send_num(uint32_t num, uint8_t radix)
@@ -49,39 +48,15 @@ void uart_send_num(uint32_t num, uint8_t radix)
 	ltoa(num, num_str, radix);
 	uart_send_string(num_str);
 }
-//
-//void uart_system_reset_msg()
-//{
-//	uint16_t count = TCC0.CNT;
-//	char *msg;
-//	if ((RST.STATUS & RST_SRF_bm) != 0)
-//		msg = "software reset\n\r";
-//	else if ((RST.STATUS & RST_PDIRF_bm) != 0)
-//		msg = "PDI reset\n\r";
-//	else if ((RST.STATUS & RST_WDRF_bm) != 0)
-//		msg = "WDT reset\n\r";
-//	else if ((RST.STATUS & RST_BORF_bm) != 0)
-//		msg = "Brownout reset\n\r";
-//	else if ((RST.STATUS & RST_PORF_bm) != 0)
-//		msg = "power on reset\n\r";
-//	else
-//		msg = "unknown reset\n\r";
-//	uart_send_string(msg);
-//	uart_send_num(count, 16);
-//	uart_send_string("- ticks from start\n\r");
-//}
-
-//void uart_send_num(uint32_t num, uint8_t radix) {return ;}
-
 
 #else
 
-void uart_init(void) {return ;}
+void uart_init(void) { return; }
 
-void uart_send_char(char c) {return ;}
+void uart_send_char(char c) { return; }
 
-void uart_send_string(char *text) {return ;}
+void uart_send_string(char *text) { return; }
 
-void uart_send_num(uint32_t num, uint8_t radix) {return ;}
+void uart_send_num(uint32_t num, uint8_t radix) { return; }
 
 #endif
--- uart/uart.h
+++ uart/uart.h
@@ -10,17 +10,11 @@
 
 #include "asf.h"
 
-#define BSEL 	16
-#define UART_BASE	USARTD0
-
 //#define UART_DEBUG
 
 void uart_init(void);
-
 void uart_send_char(char c);
-
 void uart_send_string(char *text);
-
 void uart_send_num(uint32_t num, uint8_t radix);
 
 #endif /* UART_UART_H_ */
