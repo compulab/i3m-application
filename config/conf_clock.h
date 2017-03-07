126a55b42cf1aefebd52fd9ed49ba8fd0bf43429
Nikita Kiryanov <nikita@compulab.co.il>
2017-03-07 14:46:54 +0200

    config: clock: trim comments
    Trim comments to reduce noise.

--- config/conf_clock.h
+++ config/conf_clock.h
@@ -46,17 +46,12 @@
 #ifndef CONF_CLOCK_H_INCLUDED
 #define CONF_CLOCK_H_INCLUDED
 
-/* Possible sysclk sources: SYSCLK_SRC_{RC2MHZ | RC32MHZ | RC32KHZ | XOSC | PLL}
- * Possible PLL sources: PLL_SRC_{XOSC | RC32MHZ | RC2MHZ}
- * Fpll = (Fclk * PLL_mul) / PLL_div
- */
 //sysclk sourced from the 2MHz internal oscilator, upscaled to 32MHz by a PLL
 #define CONFIG_SYSCLK_SOURCE        SYSCLK_SRC_PLL
 #define CONFIG_PLL0_SOURCE			PLL_SRC_RC2MHZ
 #define CONFIG_PLL0_MUL             16
 #define CONFIG_PLL0_DIV             1
 
-/* Fbus = Fsys / (2 ^ BUS_div) */
 //Do not apply prescaler on sysclk output. Thus, all peripherals, RAM, NVM, and
 //CPU are clocked using unmodified sysclk
 #define CONFIG_SYSCLK_PSADIV          SYSCLK_PSADIV_1
@@ -68,15 +63,7 @@
 
 /* The following configures the USB controller clock
  * source to use the internal (nominally) 32MHz RC oscillator, up-calibrated to
- * run at 48MHz via the periodic 1ms USB Start Of Frame packets sent by the
- * host. The USB controller requires 48MHz for Full Speed operation, or 6MHz
- * for USB Low Speed operation.
- *
- * Note that when the 32MHz RC oscillator is tuned to 48MHz, it cannot be used
- * directly as the system clock source; it must either be prescaled down to a
- * speed below the maximum operating frequency given in the datasheet, or an
- * alternative clock source (e.g. the internal 2MHz RC Oscillator, multiplied
- * to a higher frequency via the internal PLL module) must be used instead.
+ * run at 48MHz via the periodic 1ms USB Start Of Frame packets sent by the host.
  */
 #define CONFIG_USBCLK_SOURCE                USBCLK_SRC_RCOSC
 #define CONFIG_OSC_RC32_CAL                 48000000UL
