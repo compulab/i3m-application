6baa7737e08e936a904c70a94c9186698fe19320
Nikita Kiryanov <nikita@compulab.co.il>
2017-03-07 14:48:22 +0200

    eeprom: cleanup
    Make code shorter and simpler where applicable.
    No functional changes.

--- eeprom/eeprom.c
+++ eeprom/eeprom.c
@@ -6,24 +6,25 @@
  */
 
 #include "eeprom_driver.h"
+#include "eeprom.h"
 
 #define EEPROM_BYTE_ADDRESS_MASK    0x1f
 void eeprom_write_byte(uint16_t reg_addr, uint8_t value)
 {
-	EEPROM_WriteByte((unsigned char)(reg_addr / EEPROM_PAGESIZE) , (unsigned char)(reg_addr & EEPROM_BYTE_ADDRESS_MASK), value);
+	uint8_t page_addr = reg_addr / EEPROM_PAGESIZE;
+	uint8_t byte_addr = reg_addr & EEPROM_BYTE_ADDRESS_MASK;
+	EEPROM_WriteByte(page_addr, byte_addr, value);
 }
 
 uint8_t eeprom_read_byte(uint16_t reg_addr)
 {
-	return EEPROM_ReadByte((unsigned char)(reg_addr / EEPROM_PAGESIZE), (unsigned char)(reg_addr & EEPROM_BYTE_ADDRESS_MASK));
+	uint8_t page_addr = reg_addr / EEPROM_PAGESIZE;
+	uint8_t byte_addr = reg_addr & EEPROM_BYTE_ADDRESS_MASK;
+	return EEPROM_ReadByte(page_addr, byte_addr);
 }
 
 void eeprom_write_str(char *str, uint16_t addr)
 {
-	uint8_t i = 0;
-	while (str[i] != '\0') {
-		eeprom_write_byte(addr, str[i]);
-		addr++;
-		i++;
-	}
+	for (uint8_t i = 0; str[i] != '\0'; i++)
+		eeprom_write_byte(addr++, str[i]);
 }
