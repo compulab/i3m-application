53e79d09759f49e1a343230d459008d23c5d2bf2
Nikita Kiryanov <nikita@compulab.co.il>
2017-04-11 21:13:26 +0300

    gfx: aciton_menu: move dmi code into its own file
    Move dmi specific code to its own file to better separate the dmi and
    non-dmi cases.

---
+++ gfx/action_menu/gfx_action_menu_dmi.h
@@ -0,0 +1,17 @@
+/*
+ * gfx_action_menu_dmi.h
+ *
+ * Created: 11-Apr-17 8:53:04 PM
+ *  Author: Nikita
+ */
+
+
+#ifndef GFX_ACTION_MENU_DMI_H_
+#define GFX_ACTION_MENU_DMI_H_
+
+extern struct gfx_action_menu dmi_menu;
+extern bool is_dmi_set;
+
+void set_dmi_menu(void);
+
+#endif /* GFX_ACTION_MENU_DMI_H_ */
