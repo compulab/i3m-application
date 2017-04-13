5052e5987d5d2aba579417d6b37f4dd5d2109e7b
Nikita Kiryanov <nikita@compulab.co.il>
2017-04-13 13:38:57 +0300

    scheduler: update_screen_task: get rid of is_menu_need_update()
    Now that the slow menu screen refresh bug has been fixed, we no longer
    need the is_menu_need_update check, which seems to have been a partial
    workaround.
    
    Remove it and all related code.

--- scheduler/update_screen_task.c
+++ scheduler/update_screen_task.c
@@ -11,45 +11,9 @@
 
 enum display_state display_state;
 
-static bool is_information_need_to_change(struct gfx_information *info)
-{
-	switch (info->info_type) {
-	case SET_SCREEN_SAVER_ENABLE:
-	case SET_SCREEN_SAVER_TYPE:
-	case SET_SCREEN_SAVER_TIME:
-	case SET_BRIGHTNESS:
-	case SHOW_SERIAL_NUMBER:
-	case SHOW_APP_VERSION:
-	case SHOW_PART_NUMBER:
-	case SHOW_MAC_ADDRESS:
-		return false;
-	default:
-		return true;
-	}
-}
-
-static bool is_menu_need_update(struct gfx_action_menu *menu)
-{
-	for (uint8_t i = 0; i < menu->menu->num_elements; i++) {
-		if (menu->actions[i].type != ACTION_TYPE_SHOW_FRAME)
-			continue;
-
-		if (menu->actions[i].frame == dashboard)
-			continue;
-
-		list_foreach(struct gfx_information_node *, menu->actions[i].frame->information_head, info_node) {
-			if (is_information_need_to_change(&info_node->information))
-				return true;
-		}
-	}
-
-	return false;
-}
-
 static void update_screen(void *data)
 {
 	if (display_state == DISPLAY_MENU) {
-		if (is_menu_need_update(present_menu))
 			gfx_action_menu_display(present_menu);
 	} else {
 		frame_present->draw(frame_present, false);
