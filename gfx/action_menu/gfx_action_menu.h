0db87f9096ab11577a3a55fbb99e1de189afc50a
Nikita Kiryanov <nikita@compulab.co.il>
2017-08-13 14:33:13 +0300

    gfx_action_menu: fix compiler wanrnings
    Fix implicit declaration compiler warnings.

--- gfx/action_menu/gfx_action_menu.h
+++ gfx/action_menu/gfx_action_menu.h
@@ -9,6 +9,7 @@
 #define GFX_GFX_ACTION_MENU_H_
 
 #include "gfx/action_menu/menu-handler.h"
+#include "gfx/gfx_frame.h"
 
 struct gfx_action_menu {
 	struct gfx_mono_menu *menu;
@@ -22,6 +23,8 @@ struct gfx_action_menu {
 extern struct gfx_action_menu *present_menu;
 
 void clear_screen(void);
+void gfx_display_msg(char *msg);
+void gfx_action_menu_init(void);
 void gfx_redraw_current_frame(void);
 void gfx_handle_key_pressed(struct gfx_action_menu *action_menu, uint8_t keycode, bool from_frame);
 
