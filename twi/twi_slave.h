116dbe1aa4d70edcb9eefaf71d020a263dcc067c
Nikita Kiryanov <nikita@compulab.co.il>
2017-11-26 17:01:38 +0200

    twi: get rid of unused defines
    TWIS_STATUS_READY and TWIS_STATUS_BUSY are not used.
    Remove them.

--- twi/twi_slave.h
+++ twi/twi_slave.h
@@ -8,10 +8,6 @@
 #ifndef TWI_SLAVE_H
 #define TWI_SLAVE_H
 
-/* Transaction status defines.*/
-#define TWIS_STATUS_READY                0x00
-#define TWIS_STATUS_BUSY                 0xff
-
 void twi_slave_init(void);
 
 void twi_slave_interrupt_handler(void);
