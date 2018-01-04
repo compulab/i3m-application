5c99c47ad50779bc0d9804540c6ddbacde95af8a
Nikita Kiryanov <nikita@compulab.co.il>
2018-01-04 15:09:41 +0200

    screens: sensors: power: display <5W on low power
    When power is low, instead of the word "LOW", display "<5W".

--- screens/sensors/power.c
+++ screens/sensors/power.c
@@ -46,7 +46,7 @@ static void sprintf_power_data(struct gfx_information *info, char *output_str)
 	else if (power >= 6 && power <= 300)
 		sprintf(output_str, "%ldW", power);
 	else
-		sprintf(output_str, "LOW");
+		sprintf(output_str, "<5W");
 }
 
 int gfx_information_init_show_power_data(struct gfx_information *info)
