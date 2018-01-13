79cabcf30912bfaacad4ed39f34c05d1aa676de6
Nikita Kiryanov <nikita@compulab.co.il>
2018-01-13 15:11:54 +0200

    Revert "screens: get rid of brightness settings"
    This reverts commit 81fc9a7245106ceaa86c4fe3b0a9878e55fc06a3.

---
+++ screens/settings/brightness.h
@@ -0,0 +1,16 @@
+/*
+ * brightness.h
+ *
+ * Created: 3/18/2017 9:33:56 PM
+ *  Author: Nikita
+ */
+
+
+#ifndef BRIGHTNESS_H_
+#define BRIGHTNESS_H_
+
+#include "gfx/gfx_components/gfx_information.h"
+
+int gfx_information_init_set_brightness(struct gfx_information *info);
+
+#endif /* BRIGHTNESS_H_ */
