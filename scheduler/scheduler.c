dc407a7e20443692b59af0f701c7326a4cd5bbcd
Nikita Kiryanov <nikita@compulab.co.il>
2018-01-25 14:11:26 +0200

    scheduler: simplify scheduler
    We have fairly simplistic task scheduling requierments. Implementing a
    time delayed round robin is more than enough, we don't need precise
    control of the exact timeout length.
    
    This simplification should fix a bug which caused screen task to
    simetimes stop execute.

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -28,7 +28,7 @@ extern struct scheduler_task screen_tick_task;
 struct tc_scheduler_task {
 	struct scheduler_task task;
 	int overlaps_count;
-	uint16_t offset;
+	double offset;
 };
 
 volatile void *tc_module = NULL;
@@ -48,71 +48,22 @@ static struct tc_scheduler_task new_tick_task(struct scheduler_task task)
 	return res;
 }
 
-static bool task_due_before_scheduled(uint8_t task_id)
-{
-	return tick_tasks_to_do[task_id].overlaps_count == 0 &&
-			(!cmp_enable || tick_tasks_to_do[task_id].offset < tc_read_cc(tc_module, TC_CCA));
-}
-
-static void schedule_closest_task(void)
-{
-	bool scheduled_task_exists = false;
-	array_foreach(struct tc_scheduler_task, tick_tasks_to_do, index) {
-		if (task_due_before_scheduled(index)) {
-			tc_write_cc(tc_module, TC_CCA, tick_tasks_to_do[index].offset);
-			scheduled_task_exists = true;
-		}
-	}
-
-	if (scheduled_task_exists) {
-		tc_enable_cc_channels(tc_module, TC_CCAEN);
-		cmp_enable = true;
-	} else {
-		tc_disable_cc_channels(tc_module, TC_CCAEN);
-		cmp_enable = false;
-	}
-}
-
 static void task_set_timer(int task_index)
 {
 	struct tc_scheduler_task *task = &tick_tasks_to_do[task_index];
-	//tc_get_resolution corresponds to ticks/sec
-	uint32_t ticks = task->task.get_recur_period() * tc_get_resolution(tc_module);
-	uint16_t ticks_to_overflow = TIMER_MAX_VALUE - tc_read_count(tc_module);
-	if (ticks < ticks_to_overflow) {
-		task->overlaps_count = 0;
-		task->offset = ticks + tc_read_count(tc_module);
-	} else {
-		ticks -= ticks_to_overflow;
-		task->overlaps_count = ticks / TIMER_MAX_VALUE + 1;
-		task->offset = ticks % TIMER_MAX_VALUE;
-	}
-}
-
-static void tc_cca_callback(void)
-{
-	/* Find expired task and add it to the work queue */
-	array_foreach(struct tc_scheduler_task, tick_tasks_to_do, index) {
-		if (tick_tasks_to_do[index].overlaps_count == 0 &&  tick_tasks_to_do[index].offset <= tc_read_count(tc_module)) {
-			insert_work(tick_tasks_to_do[index].task.work);
-			task_set_timer(index);
-		}
-	}
-
-	schedule_closest_task();
+	task->offset = task->task.get_recur_period();
 }
 
 static void tc_overflow_callback(void)
 {
 	tc_clear_overflow(tc_module);
 	array_foreach(struct tc_scheduler_task, tick_tasks_to_do, index) {
-		if (tick_tasks_to_do[index].overlaps_count <= 0)
-			continue;
-		else
-			tick_tasks_to_do[index].overlaps_count--;
+		tick_tasks_to_do[index].offset -= 0.1;
+		if (tick_tasks_to_do[index].offset <= 0) {
+			insert_work(tick_tasks_to_do[index].task.work);
+			task_set_timer(index);
+		}
 	}
-
-	schedule_closest_task();
 }
 
 void scheduler_init(volatile void *tc)
@@ -120,8 +71,6 @@ void scheduler_init(volatile void *tc)
 	tc_module = tc;
 	tc_set_overflow_interrupt_callback(tc_module, tc_overflow_callback);
 	tc_set_overflow_interrupt_level(tc_module, TC_INT_LVL_HI);
-	tc_set_cca_interrupt_callback(tc_module, tc_cca_callback);
-	tc_set_cca_interrupt_level(tc_module, TC_INT_LVL_HI);
 
     tick_tasks_to_do[0] = new_tick_task(pending_req_tick_task);
 	tick_tasks_to_do[1] = new_tick_task(adc_tick_task);
@@ -129,6 +78,4 @@ void scheduler_init(volatile void *tc)
 
 	array_foreach(struct tc_scheduler_task, tick_tasks_to_do, index)
 		task_set_timer(index);
-
-	schedule_closest_task();
 }
